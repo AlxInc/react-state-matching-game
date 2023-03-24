64eb4871341ec485ff18b951a648ad84725fd33c
AlxInc <108522772+AlxInc@users.noreply.github.com>
2023-03-24 15:39:33 +1030

    another typo

--- src/components/TileSelector/TileSelector.js
+++ src/components/TileSelector/TileSelector.js
@@ -8,9 +8,9 @@ const TileSelector = (props) => {
 
   const dropdown = hovered ? (
         <div className='tileSelectorContent' >
-          <div className='number' onClick={() => props.hanfleNumTileChange(4)}>4</div>
-          <div className='number' onClick={() => props.hanfleNumTileChange(16)}>16</div>
-          <div className='number' onClick={() => props.hanfleNumTileChange(36)}>36</div>
+          <div className='number' onClick={() => props.handleNumTileChange(4)}>4</div>
+          <div className='number' onClick={() => props.handleNumTileChange(16)}>16</div>
+          <div className='number' onClick={() => props.handleNumTileChange(36)}>36</div>
         </div>
   ) : null
 
