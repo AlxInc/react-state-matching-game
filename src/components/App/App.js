a32e18dec04e540b72d04042853483cbedf2c811
AlxInc <108522772+AlxInc@users.noreply.github.com>
2023-03-24 13:08:23 +1030

    fixed order of operation

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -25,26 +25,26 @@ class App extends Component{
       const selectedTileIndex = indexOfSelected(tiles, id, color)
       let previousTileIndex = state.previousTileIndex
 
+      if(toBeCleared !== null) {
+        tiles[toBeCleared[0]].selected = false
+        tiles[toBeCleared[1]].selected = false
+        toBeCleared = null
+      }
+
+      tiles[selectedTileIndex].selected = true
+
       if(previousTileIndex !== null) {
-          const previousTile = tiles[previousTileIndex]
-          const selectedTile = tiles[selectedTileIndex]
+        const previousTile = tiles[previousTileIndex]
+        const selectedTile = tiles[selectedTileIndex]
 
-          if(toBeCleared !== null) {
-            tiles[toBeCleared[0]].selected = false
-            tiles[toBeCleared[1]].selected = false
-            toBeCleared = null
-          }
-
-          tiles[selectedTileIndex].selected = true
-
-          if(previousTile.id !== selectedTile.id && previousTile.color === color) {
-            selectedTile.matched = true
-            previousTile.matched = true
-            previousTileIndex = null
-          } else {
-            toBeCleared = [previousTileIndex, selectedTileIndex]
-            previousTileIndex = null
-          }
+        if(previousTile.id !== selectedTile.id && previousTile.color === color) {
+          selectedTile.matched = true
+          previousTile.matched = true
+          previousTileIndex = null
+        } else {
+          toBeCleared = [previousTileIndex, selectedTileIndex]
+          previousTileIndex = null
+        }
       } else {
         previousTileIndex = selectedTileIndex
       }
