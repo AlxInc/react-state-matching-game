9a3ff751dfdbe328a0b08ff0e8040b2a752f09f3
AlxInc <108522772+AlxInc@users.noreply.github.com>
2023-03-24 16:16:16 +1030

    another typo

--- refactor/GameContext.js
+++ refactor/GameContext.js
@@ -2,7 +2,7 @@ import React from 'react'
 
 const GameContext = React.createContext({
     numTiles: 36,
-    player: false,
+    playing: false,
     handleNumTileChange: () => {},
     startPlaying: () => {}
 })
